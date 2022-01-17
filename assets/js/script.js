44d1b200177706266a621f7fd62b51a5aa1387d6
Amy Rodriguez <queenamyrodriguez@gmail.com>
2022-01-16 17:36:29 -0700

    add drop to delete funcionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -235,3 +235,16 @@ $(".card .list-group").sortable({
     console.log(tempArr);
   }
 });
+$("#trash").droppable({
+  accept: ".card .list-group-item",
+  tolerance: "touch",
+  drop: function(event, ui) {
+    ui.draggable.remove();
+  },
+  over: function(event, ui) {
+    console.log("over");
+  },
+  out: function(event, ui) {
+    console.log("out");
+  }
+});
